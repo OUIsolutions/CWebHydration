41ba6481548558e6a6a44021fa8a355f4dffca0c
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-07-04 04:23:02 -0300

    att

---
+++ src/one.c
@@ -0,0 +1,7 @@
+
+#ifndef CWEB_HYDRATION
+#define CWEB_HYDRATION
+#include "declaration.h"
+#include "definition.c"
+
+#endif
---
+++ teste.c
@@ -0,0 +1,6 @@
+
+#include "CWebStudio.h"
+#include "src/one.c"
+int main() {
+
+}
