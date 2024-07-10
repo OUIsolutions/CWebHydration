6bf90a35c17a23077e18a59be462e3318f2b584b
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-07-09 22:31:07 -0300

    att

--- src/entries/entrie/entrie.h
+++ src/entries/entrie/entrie.h
@@ -1,12 +1,11 @@
 
 
 typedef struct privateCWebHyDrationEntrie {
-    char *key;
-    int type;
-    bool required;
     char * script;
 }privateCWebHyDrationEntrie;
 
 privateCWebHyDrationEntrie *private_privateCWebHyDrationEntrie();
+
+
 
 void privateCWebHyDrationEntrie_free(privateCWebHyDrationEntrie*self);
--- teste.c
+++ teste.c
@@ -1,16 +1,48 @@
 
 #include "CWebStudio.h"
-#include "release/CWebHyDration.h"
+#include "src/one.c"
 CwebNamespace cweb;
+CTextNamespace stack;
 
+CwebHttpResponse *server(CwebHttpRequest *request) {
 
+    CWebHyDration *hy = newCWebHyDration(request);
+
+    CWebHyDrationBridge *increment = CWebHyDration_create_bridge(hy,"/increment",NULL);
+
+    if(CWebHyDrationBridge_is_the_route(increment)) {
+        long num = CWebHyDration_get_num(hy,"num");
+
+        if(CWebHyDration_error(hy)) {
+            char *error = CWebHydration_get_error(hy);
+            return  cweb.response.send_text(error,404);
+        }
+
+        CTextStack * text = newCTextStack(CTEXT_LINE_BREAKER,CTEXT_SEPARATOR);
+        CTextScope(text,CTEXT_H3,{
+            CTextFormat(text,"%d",num + num);
+        })
+
+        CWebHyDration_replace_id_with_ctext_cleaning_memory("h3",text);
+
+    }
+
+    CTextStack * text = newCTextStack(CTEXT_LINE_BREAKER,CTEXT_SEPARATOR);
+    CTextScope(text,CTEXT_BODY,{
+
+        CTextScope(text,CTEXT_H3,{
+            CTextFormat(text,"%d",0);
+        })
+        CTextScope(text,CTEXT_BUTTON,{
+            CTextFormat(text,"increment");
+        })
+    })
+
+    return  cweb.response.send_rendered_CTextStack_cleaning_memory(text,200);
+}
 int main(int argc, char *argv[]){
     cweb = newCwebNamespace();
-
-    CWebHyDration *hy = newCWebHyDration(NULL);
-
-    CWebHyDrationBridge *teste = CWebHyDration_create_bridge(hy,"/teste",NULL);
-    CWebHyDrationBridge *teste2 = CWebHyDration_create_bridge(hy,"/teste2",NULL);
+    stack = newCTextNamespace();
 
     char* script  = CWebHyDration_create_script(hy);
     printf("%s\n",script);
