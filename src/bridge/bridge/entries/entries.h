31f66b7e91d5beb923481da7ab85f3c5a4d99893
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-07-10 00:20:33 -0300

    .h

--- src/bridge/bridge/entries/entries.h
+++ src/bridge/bridge/entries/entries.h
@@ -2,5 +2,14 @@
 
 void CWebHyDrationBridge_add_function(CWebHyDrationBridge *self,const char *function,const char *format, ...);
 
-
 void CWebHyDrationBridge_add_input_by_id(CWebHyDrationBridge *self,const char *id);
+
+void CWebHyDrationBridge_add_input_checkbox_by_id(CWebHyDrationBridge *self, const char *id);
+
+void CWebHyDrationBridge_request_text_cont_by_id(CWebHyDrationBridge *self, const char *id);
+
+void CWebHyDrationBridge_add_input_by_all_id(CWebHyDrationBridge *self, const char *id);
+
+void CWebHyDrationBridge_add_input_checkbox_by_all_id(CWebHyDrationBridge *self, const char *id);
+
+void CWebHyDrationBridge_request_text_cont_by_all_id(CWebHyDrationBridge *self, const char *id);
