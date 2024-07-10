e7508f149f087176cfd6531d1157df00cc303a94
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-07-10 05:10:35 -0300

    att

--- js/callbacks.js
+++ js/callbacks.js
@@ -2,26 +2,11 @@
 
 let private_cweb_callbacks = {}
 
-/**
- *@typedef {Object} AlertArgs
- *@property {string} menssage
- */
-
-/**
- *@param {AlertArgs} args
- **/
 private_cweb_callbacks.private_cweb_alert = function (args){
     alert(args.menssage);
 }
 
-/**
- *@typedef {Object} DestroyArgs
- *@property {string} id
- */
 
-/**
- *@param {DestroyArgs} args
- * **/
 private_cweb_callbacks.private_cweb_destroy_by_id = function (args){
     let element = document.getElementById(args.id);
     if(element){
@@ -29,15 +14,9 @@ private_cweb_callbacks.private_cweb_destroy_by_id = function (args){
     }
 }
 
-/**
- *@typedef {Object} ReplaceIdArg
- *@property {string} id
- *@property {string} html
- */
 
-/**
- * @param {ReplaceId} args
- * **/
+
+
 private_cweb_callbacks.private_cweb_hydration_replace_by_id = function (args){
     let element = document.getElementById(args.id);
     if(!element){
--- js/sender.js
+++ js/sender.js
@@ -2,17 +2,6 @@
 
 
 
-/**
- *@typedef {Object} ServerResponse
- *@property {string} callback
- *@property {Object} args
- */
-
-
-/**
- *@param {string} route
- *@param {object} body
- **/
 async function private_cweb_send_to_server(route,body){
 
     let result = await fetch(route,{
@@ -23,7 +12,6 @@ async function private_cweb_send_to_server(route,body){
      console.log(result.text());
     return;
  }
- /**@type {Array<ServerResponse>}**/
  let parsed =await result.json();
  parsed.forEach(function (callback){
      try{
